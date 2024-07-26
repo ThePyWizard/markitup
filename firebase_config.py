b95238755ed1f538a8c51056f1f2c76e26e40c7d
Jishnu M M <jishnumm4312@gmail.com>
2024-07-26 16:46:18 +0530

    restarted work

--- firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase
-cred = credentials.Certificate("path/to/your/firebase-adminsdk.json")
-firebase_admin.initialize_app(cred)
-
-# Initialize Firestore
-db = firestore.client()
